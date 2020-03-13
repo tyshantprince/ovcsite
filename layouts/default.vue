82440cd77d62263d5efd372f37c241404ece3eee
tyshantprince <tyshantprince@gmail.com>
2020-03-13 15:54:06 -0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+      <v-list>
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar class tile app dense>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-container>
+        <v-layout>
+          <v-img
+            class="mr-5"
+            src="ovc.png"
+            height="70"
+            width="80"
+            @click="$vuetify.goTo(0)"
+          />
+          <v-btn
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            class="hidden-sm-and-down ma-3"
+            text
+            large
+          >{{ item.title }}</v-btn>
+          <v-spacer />
+        </v-layout>
+      </v-container>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Home",
+          to: "/"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Get Started",
+          to: "/get-started"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Legal Needs",
+          to: "/legal-needs"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Find Help",
+          to: "/find-help"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Glossary",
+          to: "/glossary"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "About Us",
+          to: "/about-us"
+        }
+      ],
+      miniVariant: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
